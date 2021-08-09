aea174b9a9e2fa1d348e46e6e2373090c3c918c5
Kevin Platko <81776002+KevinPlatko@users.noreply.github.com>
2021-08-08 21:28:18 -0400

    Update sys_info.sh

--- Linux/sys_info.sh
+++ Linux/sys_info.sh
@@ -1,26 +1,47 @@
 #!/bin/bash
 
-mkdir ~/research 2>/dev/null
+#Check if script was run as root. Exit if true.
+if [ $UID -eq 0 ]; then
+  echo "Please do not run this script as root."
+  exit
+fi
 
-echo "A Quick System Audit Script" >~/research/sys_info.txt
-date >>~/research/sys_info.txt
-echo "" >>~/research/sys_info.txt
-echo "Machine Type Info:" >>~/research/sys_info.txt
-echo $MACHTYPE >>~/research/sys_info.txt
-echo -e "Uname info: $(uname -a) \n" >>~/research/sys_info.txt
-echo -e "IP Info: $(ip addr | grep inet | tail -2 | head -1) \n" >>~/research/sys_info.txt
-echo -e "Hostname: $(hostname -s) \n" >>~/research/sys_info.txt
-echo "DNS Servers: " >>~/research/sys_info.txt
-cat /etc/resolv.conf >>~/research/sys_info.txt
-echo -e "\nMemory Info:" >>~/research/sys_info.txt
-free >>~/research/sys_info.txt
-echo -e "\nCPU Info:" >>~/research/sys_info.txt
-lscpu | grep CPU >>~/research/sys_info.txt
-echo -e "\nDisk Usage:" >>~/research/sys_info.txt
-df -H | head -2 >>~/research/sys_info.txt
-echo -e "\nWho is logged in: \n $(who -a) \n" >>~/research/sys_info.txt
-echo -e "\nExec Files:" >>~/research/sys_info.txt
-find /home -type f -perm 777 >>~/research/sys_info.txt
-echo -e "\nTop 10 Processes" >>~/research/sys_info.txt
-ps aux -m | awk {'print $1, $2, $3, $4, $11'} | head >>~/research/sys_info.txt
+# Define Variables
+output=$HOME/research/sys_info.txt
+ip=$(ip addr | grep inet | tail -2 | head -1)
+execs=$(sudo find /home -type f -perm 777 2>/dev/null)
+
+# Check for research directory. Create it if needed.
+if [ ! -d $HOME/research ]; then
+  mkdir $HOME/research
+fi
+
+# Check for output file. Clear it if needed.
+if [ -f $output ]; then
+  rm $output
+fi
+
+echo "A Quick System Audit Script" >>$output
+date >>$output
+echo "" >>$output
+echo "Machine Type Info:" >>$output
+echo -e "$MACHTYPE \n" >>$output
+echo -e "Uname info: $(uname -a) \n" >>$output
+echo -e "IP Info:" >>$output
+echo -e "$ip \n" >>$output
+echo -e "Hostname: $(hostname -s) \n" >>$output
+echo "DNS Servers: " >>$output
+cat /etc/resolv.conf >>$output
+echo -e "\nMemory Info:" >>$output
+free >>$output
+echo -e "\nCPU Info:" >>$output
+lscpu | grep CPU >>$output
+echo -e "\nDisk Usage:" >>$output
+df -H | head -2 >>$output
+echo -e "\nWho is logged in: \n $(who -a) \n" >>$output
+echo -e "\nexec Files:" >>$output
+echo $execs >>$output
+echo -e "\nTop 10 Processes" >>$output
+ps aux --sort -%mem | awk {'print $1, $2, $3, $4, $11'} | head >>$output
+
 
